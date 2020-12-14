a3887fdf322c4e938517a92c90470f49148cfe1f
lois <flojuve@hotmail.fr>
2020-12-14 18:13:11 +0100

    .

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FlickrService } from '../flickr.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  images = [];
+  keyword: string;
+
+  constructor(private flickrService: FlickrService) { }
+
+  ngOnInit(): void {
+  }
+
+  search(event: any) {
+    this.keyword = event.target.value.toLowerCase();
+    if (this.keyword && this.keyword.length > 0) {
+      this.flickrService.search_keyword(this.keyword)
+      .toPromise()
+      .then(res => {
+        this.images = res;
+      })
+    }
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  flickr: {
+    key: "69cec16e99e8e1238e14cc9734536c29"
+  }
 };
 
 /*
